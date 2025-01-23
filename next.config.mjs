cbafaa21c322c835f7311b9ad251217e79fd1554
Tamim Ahmed <tamim20072@gmail.com>
2025-01-24 04:22:17 +0600

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // This disables ESLint checks during builds
+    },
+  };
+  
+  module.exports = nextConfig; // Use this to export the configuration
+  
